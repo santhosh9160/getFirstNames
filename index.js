225ab263fabdaa25856e6b40b324855c253bd67a
santhosh <santhosh1995b@gmail.com>
2023-05-24 08:27:10 +0000

    Get First Names

---
+++ index.js
@@ -0,0 +1,8 @@
+const peopleNames = require("../country/state/city/index");
+const getFirstNames = require("../utilities/utils/index");
+
+const getPeopleInCity = (peopleNames) => {
+  return getFirstNames(peopleNames);
+};
+console.log(getPeopleInCity);
+module.exports = getPeopleInCity;
